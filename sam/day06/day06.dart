de7791a48d5bbe6e5edb68e311c5e8fa038d56b2
Samuel Fuller <fullsam@amazon.com>
2025-12-26 10:37:55 +0000

    Complete part 2 of Day 06

--- sam/day06/day06.dart
+++ sam/day06/day06.dart
@@ -25,9 +25,49 @@ int part1(List<String> input) {
   return total;
 }
 
+int part2(List<String> input) {
+  final cells = RegExp(r'[+*]\s{1,5}')
+      .allMatches(input.last)
+      .map((match) {
+        var startIndex = match.start;
+        var endIndex = match.end;
+
+        return [
+          input[0].substring(startIndex, endIndex),
+          input[1].substring(startIndex, endIndex),
+          input[2].substring(startIndex, endIndex),
+          input[3].substring(startIndex, endIndex),
+          input[4][startIndex],
+        ];
+      })
+      .toList(growable: false);
+
+  return cells
+      .map((column) {
+        final operation = column.last;
+        final List<int> numbers = [];
+
+        for (var i = 0; i < column.first.length; i++) {
+          final num = int.tryParse(
+            (column[0][i] + column[1][i] + column[2][i] + column[3][i]).trim(),
+          );
+
+          if (num != null) {
+            numbers.add(num);
+          }
+        }
+
+        return numbers.reduce(
+          (acc, curr) => operation == '*' ? acc * curr : acc + curr,
+        );
+      })
+      .reduce((acc, elem) => acc + elem);
+}
+
 void main() {
   final file = File('input.txt');
   final lines = file.readAsLinesSync();
 
   print(part1(lines));
+  print(part2(lines));
 }
